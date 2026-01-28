8ddcf01f780fce328a7c499bd6b62b4ab5ea4996
18003404183 <1084233733@QQ.com>
2026-01-28 23:23:39 +0800

    新增 Danmaku 和 Collider 类的头文件，定义基本结构和注释

---
+++ src/Entities/Danmaku.h
@@ -0,0 +1,43 @@
+// IRenderable IUpdateable IEntity 继承以下接口
+// 不用继承接口 防止多余开销
+class Danmaku{
+public:
+    //Danmaku b = new Danmaku(a)
+    //b = a
+    Danmaku() = default;
+
+    Danmaku(const Danmaku& other){
+
+    }
+
+    Danmaku& operator=(const Danmaku& other){
+
+    }
+
+    // 实现update方法
+
+    // 实现renderable方法
+
+    // 实现碰撞体的方法
+
+    //各种get set 方法
+
+    ~Danmaku() = default; 
+
+private:
+
+// 弹幕的坐标 (vector<glm::vec2>) 为了让有些多坐标弹幕能够确定位置(比如激光需要起始坐标与终止坐标)
+    
+// image 负责弹幕的贴图
+
+// 碰撞体接口 负责弹幕的碰撞检测 
+
+// 弹幕的类型
+
+// 运动函数接口
+
+// 附加数据 使用Vector<float> 
+
+//  
+
+};
